db06fe4e96374664748a45f728248adf4d5b2b60
Dmitry <dmitrief@gmail.com>
2020-12-21 02:02:08 -0700

    chore(docs): update sample

--- projects/demo/src/app/samples/basic.ts
+++ projects/demo/src/app/samples/basic.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   geolocationState$ = this.permissionsService.state('geolocation');
 
   constructor(
-    @Inject(PermissionsService) readonly permissionsService: PermissionsService,
+    private readonly permissionsService: PermissionsService,
   ) {}
 }
 `;
