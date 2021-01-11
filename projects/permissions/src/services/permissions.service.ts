b72be71f672b775e4fb7759ceb4cfd4645818e3a
MarsiBarsi <darragon-nn@yandex.ru>
2021-01-11 21:17:59 +0300

    chore(permissons): add dynamic

--- projects/permissions/src/services/permissions.service.ts
+++ projects/permissions/src/services/permissions.service.ts
@@ -6,6 +6,7 @@ import {PERMISSIONS_SUPPORT} from '../tokens/permissions-support';
 
 export type PermissionsQueryArgs = Parameters<typeof Permissions.prototype.query>[0];
 
+// @dynamic
 @Injectable({
     providedIn: 'root',
 })
